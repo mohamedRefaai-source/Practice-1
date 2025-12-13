c47f3c4086a6f81b9fb6edae1ad855617275f14f
Mohamed Refaai <mohamed.refaai@icpcredit.com>
2025-12-13 12:42:51 +0200

    code

---
+++ script.js
@@ -0,0 +1,42 @@
+const plansCards = document.querySelectorAll(".card");
+plansCards.forEach((card) => {
+  card.addEventListener("click", () => {
+    plansCards.forEach((c) => {
+      if (c.classList.contains("highlight")) {
+        c.classList.toggle("highlight");
+      }
+    });
+    card.classList.toggle("highlight");
+  });
+});
+
+function toggleHighlight(clientsCard) {
+  clientsCards.forEach((c) => {
+    if (c.classList.contains("highlight-client-card")) {
+      c.classList.remove("highlight-client-card");
+    }
+  });
+  clientsCard.classList.add("highlight-client-card");
+}
+const clientsCards = document.querySelectorAll(".client-card");
+
+clientsCards.forEach((clientsCard) => {
+  clientsCard.addEventListener("mouseover", () => toggleHighlight(clientsCard));
+});
+
+// const headerButtons = document.querySelector(".pricing-button");
+// const dropList = document.querySelector(".drop-down-list");
+// headerButtons.addEventListener("click", () => {
+//   dropList.classList.toggle("show");
+// });
+
+const headerButtons = document.querySelectorAll(".drop-down-button");
+
+headerButtons.forEach((btn) => {
+  btn.addEventListener("mouseover", () => {
+    btn.children[0].classList.add("show");
+  });
+  btn.addEventListener("mouseout", () => {
+    btn.children[0].classList.remove("show");
+  });
+});
